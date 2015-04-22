b8362c37047d3877fce2f761fb308063879e6bcd
kiergarlen <oscar_igm@hotmail.com>
2015-04-22 16:59:25 -0500

    Sheet validation draft

--- js/controllers_wip.js
+++ js/controllers_wip.js
@@ -6,7 +6,10 @@
    * @this {Object} $scope - Contenedor para el modelo [AngularJS]
    * @param {Object} $routeParams - Proveedor de parámetros de ruta [AngularJS]
    * @param {Object} TokenService - Proveedor para manejo del token
+   * @param {Object} ValidationService - Proveedor para manejo de validación
+   * @param {Object} RestUtilsService - Proveedor para manejo de servicios REST
    * @param {Object} ArrayUtilsService - Proveedor para manejo de arreglos
+   * @param {Object} DateUtilsService - Proveedor para manejo de fechas
    * @param {Object} CloudService - Proveedor de datos, Coberturas nubes
    * @param {Object} WindService - Proveedor de datos, Direcciones viento
    * @param {Object} WaveService - Proveedor de datos, Intensidades oleaje
@@ -16,10 +19,11 @@
    * @param {Object} PreservationService - Proveedor de datos, Preservaciones
    * @param {Object} SheetService - Proveedor de datos, Hojas de campo
    */
-  function SheetController($routeParams, TokenService, ArrayUtilsService,
-    CloudService, WindService, WaveService,
-    SamplingNormService, PointService, FieldParameterService,
-    PreservationService, SheetService) {
+  function SheetController($routeParams, TokenService, ValidationService,
+    ArrayUtilsService, DateUtilsService, CloudService,
+    WindService, WaveService, SamplingNormService,
+    PointService, FieldParameterService, PreservationService,
+    SheetService) {
     var vm = this;
     vm.user = TokenService.getUserFromToken();
     vm.sheet = SheetService.query({sheetId: $routeParams.sheetId});
@@ -28,7 +32,6 @@
     vm.waveIntensities = WaveService.get();
     vm.samplingNorms = SamplingNormService.get();
     vm.preservations = PreservationService.get();
-    vm.isPreservationListLoaded = false;
     /*
     SheetService
       .query({sheetId: $routeParams.sheetId})
@@ -37,7 +40,12 @@
         vm.sheet = response;
       });
     */
+    vm.isPreservationListLoaded = false;
+    vm.isDataSubmitted = false;
     vm.selectPreservations = selectPreservations;
+    vm.isResultListValid = isResultListValid;
+    vm.approveItem = approveItem;
+    vm.rejectItem = rejectItem;
     vm.isFormValid = isFormValid;
     vm.submitForm = submitForm;
 
@@ -47,7 +55,6 @@
       {
         if (vm.sheet.preservaciones.length > 0 && !vm.isPreservationListLoaded)
         {
-          console.log('forst');
           ArrayUtilsService.seItemsFromReference(
             vm.preservations,
             vm.sheet.preservaciones,
@@ -70,22 +77,151 @@
       }
     }
 
-    function isFormValid() {
+    function approveItem() {
+      ValidationService.approveItem(vm.sheet, vm.user);
+    }
 
+    function rejectItem() {
+      ValidationService.rejectItem(vm.sheet, vm.user);
+    }
+
+    function isResultListValid() {
+      var i = 0,
+      j = 0,
+      l = 0,
+      m = 0,
+      samples = [],
+      results = [];
+      if (vm.sheet.muestras && vm.sheet.muestras.length > 0)
+      {
+        samples = vm.sheet.muestras;
+        l = samples.length;
+        for (i = 0; i < l; i += 1) {
+          if (!DateUtilsService.isValidDate(new Date(samples[i].fecha_muestreo)))
+          {
+            vm.message += ' Ingrese una fecha/hora válida para el punto ';
+            vm.message += samples[i].punto + ' ';
+            return false;
+          }
+          console.log(samples[i].resultados.slice());
+          break;
+            /*
+          results = samples[i].resultados.slice();
+          m = results.length;
+          for (j = 0; j < m; j += 1) {
+            if (isNaN(samples[i].cantidad) || samples[i].cantidad < 1)
+            {
+              vm.message += ' Ingrese cantidad, para el reactivo ';
+              vm.message += '(Ver fila ' + (i + 1) + ')';
+              return false;
+            }
+            if (isNaN(samples[i].lote) || samples[i].lote < 1)
+            {
+              vm.message += ' Ingrese lote, para el reactivo ';
+              vm.message += '(Ver fila ' + (i + 1) + ')';
+              return false;
+            }
+          }
+            */
+        }
+      }
+      else
+      {
+        vm.message += ' No hay resultados ';
+        return false;
+      }
+      return true;
+    }
+
+    function isFormValid() {
+      vm.message = '';
+      if (vm.sheet.id_metodo_muestreo < 1)
+      {
+        vm.message += ' Seleccione una Norma de referencia ';
+        return false;
+      }
+      if (!DateUtilsService.isValidDate(new Date(vm.sheet.fecha_inicio)))
+      {
+        vm.message += ' Ingrese una fecha/hora válida de muestreo ';
+        return false;
+      }
+      if (!vm.isResultListValid)
+      {
+        return false;
+      }
+      /*
+      if (vm.sheet.id_nubes < 1)
+      {
+        vm.message += ' Seleccione una cobertura de nubes ';
+        return false;
+      }
+      if (vm.sheet.id_direccion_corriente < 1)
+      {
+        vm.message += ' Seleccione una dirección de corriente ';
+        return false;
+      }
+      if (vm.sheet.id_oleaje < 1)
+      {
+        vm.message += ' Seleccione una intensidad del oleaje ';
+        return false;
+      }
+      if (vm.sheet.preservaciones.length < 1)
+      {
+        vm.message += ' Seleccione al menos un tipo de preservación ';
+        return false;
+      }
+      if (vm.user.level < 3)
+      {
+        if (vm.sheet.id_status == 3 && vm.sheet.motivo_rechaza.length < 1)
+        {
+          vm.message += ' Ingrese el motivo de rechazo del Informe ';
+          return false;
+        }
+      }
+      */
+      return true;
     }
 
     function submitForm() {
-
+      if (isFormValid() && !vm.isDataSubmitted)
+      {
+        console.log(vm.sheet);
+        vm.isDataSubmitted = true;
+        if (vm.sheet.id_hoja > 0)
+        {
+          RestUtilsService
+            .saveData(
+              SheetService,
+              vm.sheet,
+              'recepcion/hoja',
+              'id_hoja'
+            );
+        }
+        else
+        {
+          if (vm.user.level < 3 || vm.sheet.sheet.id_status < 2)
+          {
+            RestUtilsService
+              .updateData(
+                SheetService,
+                vm.sheet,
+                'recepcion/hoja',
+                'id_hoja'
+              );
+          }
+        }
+      }
     }
   }
   angular
     .module('sislabApp')
     .controller('SheetController',
       [
-        '$routeParams', 'TokenService', 'ArrayUtilsService',
-        'CloudService', 'WindService', 'WaveService',
-        'SamplingNormService', 'PointService', 'FieldParameterService',
-        'PreservationService', 'SheetService',
+        '$routeParams', 'TokenService', 'ValidationService',
+        'ArrayUtilsService', 'DateUtilsService', 'CloudService',
+        'WindService', 'WaveService', 'SamplingNormService',
+        'PointService', 'FieldParameterService', 'PreservationService',
+        'SheetService',
         SheetController
       ]
     );
@@ -106,7 +242,7 @@
     vm.reception = ReceptionService.query({receptionId: $routeParams.sheetId});
 
     vm.selectReceptionist = selectReceptionist;
-    vm.validateReceptionForm = validateReceptionForm;
+    vm.validateForm = validateForm;
     vm.submitReceptionForm = submitReceptionForm;
 
     function selectReceptionist(idRecepcionist) {
@@ -122,7 +258,7 @@
       return vm.reception.recepcionista;
     }
 
-    function validateReceptionForm() {
+    function validateForm() {
 
     }
 
